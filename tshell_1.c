161024bb7f42c9ad67b84fcab0c3f64e8e1f9d2a
Karine Mellata <karine.mellata@shopify.com>
2018-09-22 15:05:46 -0400

    first commit

---
+++ tshell_1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char * get_a_line(void)
+{
+	char * line = malloc(100), * linep = line;
+	size_t lenmax = 100, len = lenmax;
+	int c;
+	
+	if(line == NULL)
+		return NULL;
+
+	for(;;){
+		c = fgetc(stdin);
+		if(c == EOF)
+			break;
+
+		if(--len == 0) {
+			len = lenmax;
+			char * linen = realloc(linep, lenmax *= 2);
+
+			if(linen == NULL) {
+				free(linep);
+				return NULL;
+			}
+			line = linen + (line - linep);
+			linep = linen;
+		}
+		
+		if((*line++ = c) == '\n')
+			break;
+	}
+	*line = '\0';
+	return linep;
+}
+
+size_t length(const char *str)
+{
+	const char *s;
+	for(s=str; *s; ++s);
+	return(s-str);
+}
+
+int main(){
+	while(1){
+		char * line = get_a_line();
+		if(length(line) > 1) {
+			system(line);
+		}
+		else{
+			exit(0);
+		}
+	}
+	return 0;
+}
