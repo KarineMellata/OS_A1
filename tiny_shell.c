99149dfaec3b80028337deda088c8e024ef53ba5
karine.mellata@mail.mcgill.ca <karine.mellata@mail.mcgill.ca>
2018-09-27 20:00:52 -0400

    automatic push

--- tiny_shell.c
+++ tiny_shell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* Read from command line and returns char pointer to string command*/
 char * get_a_line(void)
@@ -47,23 +48,41 @@ size_t length(const char *str)
 int my_system(const char *command)
 {
 	#ifdef FORK
-		pid_t childPid;
+		pid_t childPid, wPid;
 		int status;
 		switch(childPid = fork()){
 			/* Error */
 			case -1:
 				status = -1; 
-				perror("Error in creating child process");
+				printf("Error in creating child process");
 				break;
 			/* Successful */
 			case 0:
-				execve("/bin/sh", "sh", "-c", command, (char *) NULL);
+				if(execl("/bin/sh", "sh", "-c", command, (char *) NULL) == -1){
+					printf("Error in exec");
+				}
+				exit(0);
 			default:
 				waitpid(childPid, &status, 0);
-			
 		}
 	#elif VFORK
-		system(command);
+		pid_t childPid, wPid;
+		int status;
+		switch(childPid = fork()){
+			/* Error */
+			case -1:
+				status = -1; 
+				printf("Error in creating child process");
+				break;
+			/* Successful */
+			case 0:
+				if(execl("/bin/sh", "sh", "-c", command, (char *) NULL) == -1){
+					printf("Error in exec");
+				}
+				exit(0);
+			default:
+				waitpid(childPid, &status, 0);
+		}
 	#elif CLONE
 		system(command);
 	#elif PIPE
@@ -74,10 +93,17 @@ int my_system(const char *command)
 		return 0;
 	}
 int main(){
+	printf("Enter your command: ");
 	while(1){
 		char * line = get_a_line();
 		if(length(line) > 1) {
+			struct timespec start, stop;
+			clock_gettime(CLOCK_REALTIME, &start);
 			my_system(line);
+			clock_gettime(CLOCK_REALTIME, &stop);
+			double time = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec) / 1E9;
+			printf("Execution of this command took %lf seconds", time);
+			
 		}
 		else{
 			exit(0);
